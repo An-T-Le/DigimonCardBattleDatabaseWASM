df3219c291c2716529648b7cc3b3d94b373c876c
ATL <an_thoai_le@hotmail.com>
2022-05-14 22:35:42 -0400

    Added card type combinations

---
+++ DigimonCardBattleDatabase/Classes/DigimonCardFusionCombinations.cs
@@ -0,0 +1,54 @@
+﻿namespace DigimonCardBattleDatabase.Classes
+{
+    public class DigimonCardFusionCombinations
+    {
+        public Dictionary<string, Dictionary<string, string>> FusionResultsDictionary { get; private set; } =  new Dictionary<string,Dictionary<string,string>>
+            {
+            { "Fire",new Dictionary<string, string>
+                {
+                    {"Fire","Option"},
+                    {"Ice","Darkness"},
+                    {"Rare","Darkness"},
+                }
+            } ,
+            { "Ice",new Dictionary<string, string>
+                {
+                    {"Fire","Rare"},
+                    {"Ice","Option"},
+                    {"Nature","Darkness"}
+                }
+            },
+            { "Nature",new Dictionary<string, string>
+                {
+                    {"Fire","Ice"},
+                    {"Ice","Rare"},
+                    {"Nature","Option"}
+                }
+            },
+            { "Darkness",new Dictionary<string, string>
+                {
+                    {"Fire","Nature"},
+                    {"Nature","Rare"},
+                    {"Darkness","Option"}
+                }
+            },
+            { "Rare",new Dictionary<string, string>
+                {
+                    {"Fire","Darkness"},
+                    {"Ice","Nature"},
+                    {"Rare","Option"}
+                }
+            },
+            { "Option",new Dictionary<string, string>
+                {
+                    {"Fire","Fire"},
+                    {"Ice","Ice"},
+                    {"Nature","Nature"},
+                    {"Darkness","Darkness"},
+                    {"Rare","Rare"},
+                    {"Option","Option"},
+                }
+            },
+        };
+    }
+}
