c9e44e638d627247ee6a792b639d786f89bd5bbd
ATL <an_thoai_le@hotmail.com>
2022-07-15 07:18:13 -0400

    Started parser

---
+++ DigimonCardBattleDatabase/Classes/FusionTreeParser.cs
@@ -0,0 +1,13 @@
+﻿namespace DigimonCardBattleDatabase.Classes
+{
+    public class FusionTreeParser
+    {
+        FusionTree _fusionTree;
+        public FusionTreeParser(FusionTree fusionTree)
+        {
+            _fusionTree = fusionTree;
+        }
+
+
+    }
+}
